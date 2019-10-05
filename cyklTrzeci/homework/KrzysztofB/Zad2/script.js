116b16a7f7657aa485462281eef03b2265d73f5b
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2019-10-06 01:08:03 +0200

    finish zad2

--- cyklTrzeci/homework/KrzysztofB/Zad2/script.js
+++ cyklTrzeci/homework/KrzysztofB/Zad2/script.js
@@ -1,1 +1,24 @@
-console.log('lets start :)')
+const slider = document.getElementById('slider')
+const sliderItems = [...slider.querySelectorAll('li')]
+let sliderItem = 0;
+
+
+slider.querySelector(".control_next").addEventListener('click', nextSlide)
+slider.querySelector(".control_prev").addEventListener('click', prevSlide)
+
+
+function nextSlide(){
+    sliderItems[sliderItem].classList.remove('show')
+    sliderItem++
+    if(sliderItem >= sliderItems.length) sliderItem = 0
+    sliderItems[sliderItem].classList.add('show')
+}
+
+function prevSlide(){
+    sliderItems[sliderItem].classList.remove('show')
+    sliderItem--
+    if(sliderItem < 0) sliderItem = sliderItems.length - 1
+    sliderItems[sliderItem].classList.add('show')
+}
+// auto slider
+setInterval(nextSlide, 3000);
